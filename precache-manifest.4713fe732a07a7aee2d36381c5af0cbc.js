13f29f3cf083f46004483834289d77331e0e7bd5
Everton Carneiro <Everton@MacBook-Pro-de-Everton.local>
2020-06-27 16:40:20 -0300

    Updates

---
+++ precache-manifest.4713fe732a07a7aee2d36381c5af0cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ae4e4f4fbef5c08ec6e3a4f396b490",
+    "url": "/react-compound-interest/index.html"
+  },
+  {
+    "revision": "36988c481cb5b2f338c4",
+    "url": "/react-compound-interest/static/css/main.2f227186.chunk.css"
+  },
+  {
+    "revision": "48267913ed20144f8064",
+    "url": "/react-compound-interest/static/js/2.b0ee8ad1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-compound-interest/static/js/2.b0ee8ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36988c481cb5b2f338c4",
+    "url": "/react-compound-interest/static/js/main.b2cc9318.chunk.js"
+  },
+  {
+    "revision": "1e0f4f7a2a73cabcdd9b",
+    "url": "/react-compound-interest/static/js/runtime-main.c59aec6f.js"
+  }
+]);
